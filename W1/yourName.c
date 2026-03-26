48ae4d6db23f67802a421abc2813cb36a0340a20
Gustavo Monteiro Greco <gustavomonteirogreco@gmail.com>
2026-03-26 17:05:43 -0300

    perguntando nome em C

---
+++ W1/yourName.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // criei um buffer (espaço na memória) para guardar o nome.
+    // aqui reservo espaço para até 49 letras + o caractere nulo final.
+    char answer[50];
+
+    printf("Digite seu nome: ");
+
+    // uso scanf para ler o que o usuário digitar.
+    // %49s limita a leitura para não estourar o tamanho do buffer.
+    scanf("%49s", answer);
+    // obs: scanf para de ler após o primeiro espaço, para ler nomes completos com espaços, se usa fgets(answer, sizeof(answer), stdin);
+
+    // %s para indicar onde a variável deve ser impressa.
+    printf("Ola, %s\n", answer);
+}
